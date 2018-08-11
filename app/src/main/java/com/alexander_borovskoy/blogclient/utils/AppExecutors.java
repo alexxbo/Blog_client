e8997266549cc4482225f19db249a20d0cc50e9f
Alexx_Bo <alexxborovskoy@gmail.com>
2018-08-11 23:00:17 +0300

    make appExecutors singleton

--- app/src/main/java/com/alexander_borovskoy/blogclient/utils/AppExecutors.java
+++ app/src/main/java/com/alexander_borovskoy/blogclient/utils/AppExecutors.java
@@ -5,35 +5,48 @@ import android.os.Handler;
 import android.support.annotation.NonNull;
 
 import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class AppExecutors {
     private static final int THREAD_COUNT = 3;
 
+    // For Singleton instantiation
+    private static final Object LOCK = new Object();
+    private static AppExecutors sInstance;
     private final Executor diskIO;
-
+    private final Executor mainThread;
     private final Executor networkIO;
 
-    private final Executor mainThread;
-
-    public AppExecutors(Executor diskIO, Executor networkIO, Executor mainThread) {
+    private AppExecutors(Executor diskIO, Executor networkIO, Executor mainThread) {
         this.diskIO = diskIO;
         this.networkIO = networkIO;
         this.mainThread = mainThread;
+    }
+
+    public static AppExecutors getInstance() {
+        if (sInstance == null) {
+            synchronized (LOCK) {
+                sInstance = new AppExecutors(Executors.newSingleThreadExecutor(),
+                        Executors.newFixedThreadPool(THREAD_COUNT),
+                        new MainThreadExecutor());
+            }
+        }
+        return sInstance;
     }
 
     public Executor diskIO() {
         return diskIO;
     }
 
-    public Executor networkIO() {
-        return networkIO;
-    }
-
     public Executor mainThread() {
         return mainThread;
     }
 
-    public static class MainThreadExecutor implements Executor {
+    public Executor networkIO() {
+        return networkIO;
+    }
+
+    private static class MainThreadExecutor implements Executor {
         private Handler mainThreadHandler = new Handler(Looper.getMainLooper());
 
         @Override
